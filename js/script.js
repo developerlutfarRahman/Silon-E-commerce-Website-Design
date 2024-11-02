6937468684a84cb534e0d4ff8a8855d4e3f7f30a
Md Lutfar Rahman <uniquelutfar@gmail.com>
2024-11-02 18:54:01 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+  // Header sticky function
+  $(window).on("scroll", function () {
+    var scroll = $(window).scrollTop();
+    if (scroll < 245) {
+      $(".main-menu").removeClass("active");
+    } else {
+      $(".main-menu").addClass("active");
+    }
+  });
+
+  // Slick Slider jq
+  $(".autoplay").slick({
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: false,
+    arrows: false,
+    autoplaySpeed: 1500,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+  // Aos Js
+  AOS.init({
+    offset: 120,
+    duration: 1000,
+  });
+});
